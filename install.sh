564237a07d6b6c933324da045cb58ef479a6e061
adamors <adamors@users.noreply.github.com>
2017-10-24 12:32:38 +0300

    Fix install script expanding $PATH

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo 'installing neovim'
 echo -e "\e[32minstalling neovim\e[0m"
 mkdir -p ~/local
 wget -O - https://github.com/hotgloupi/nvim/releases/download/0.7/nvim-centos6.tgz | tar xjf - -C ~/local
-echo "PATH=~/local/bin:$PATH" >> ~/.profile
+echo "PATH=~/local/bin:\$PATH" >> ~/.profile
 
 echo -e "\e[32msetting up neovim config\e[0m"
 mkdir -p ~/.config
