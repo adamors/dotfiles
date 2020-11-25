993d316a38846d6dea2273929b6cc945f17a7e6f
adamors <adamors@users.noreply.github.com>
2020-11-25 21:29:26 +0200

    Don't autoload tmux in fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,5 @@
 set my_aliases "$HOME/.config/fish/functions/aliases.fish"
 set my_local_config "$HOME/.config/fish/local_config.fish"
-set tmux_config "$HOME/.config/fish/tmux_config.fish"
 
 source $my_aliases
 
@@ -8,8 +7,5 @@ if test -e $my_local_config
     source $my_local_config
 end
 
-if test -e $tmux_config
-    source $tmux_config
-end
-
 set -Ux EDITOR nvim
+eval (direnv hook fish)
