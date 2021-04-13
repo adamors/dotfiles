85843a8cffb5ee2e73b87ed9a279a0613518987a
adamors <adamors@users.noreply.github.com>
2021-04-13 13:05:27 +0300

    Fish iterm integration

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,15 @@
 set my_aliases "$HOME/.config/fish/functions/aliases.fish"
 set my_local_config "$HOME/.config/fish/local_config.fish"
+set iterm2_shell_integration "$HOME/.iterm2_shell_integration.fish"
 
 source $my_aliases
 
 if test -e $my_local_config
     source $my_local_config
+end
+
+if test -e $iterm2_shell_integration
+#     source $iterm2_shell_integration
 end
 
 set -Ux EDITOR nvim
